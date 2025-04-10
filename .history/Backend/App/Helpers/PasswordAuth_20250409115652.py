53083217fcdc28a16d887258de51d091a4ecbd2c
JonnyMast <jonny.hugoey@gmail.com>
2025-04-10 13:36:11 +0200

    linter_jonny

--- .history/Backend/App/Helpers/PasswordAuth_20250409115652.py
+++ .history/Backend/App/Helpers/PasswordAuth_20250409115652.py
@@ -6,12 +6,14 @@ class PasswordAuth:
         """This function is for creating a hash from a password for storing in database"""
         try:
             return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()).decode("utf-8")
-
+        except:
+            return None
         
     def CheckPass(self, input_pass:str, database_pass:str) -> bool:
         """This function checks raw input 'pass123' up with the hash stored in the database '98jv8aqou4rf7'"""
         try: 
             return bcrypt.checkpw(input_pass.encode("utf-8"), database_pass.encode("utf-8"))
-
+        except:
+            return False
         
 password_auth = PasswordAuth()
